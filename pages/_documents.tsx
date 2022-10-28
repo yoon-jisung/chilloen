d498e0ea94d7bc3870b0b698020f1a695f95d99f
yoon-jisung <hmsj2380@gmail.com>
2022-10-28 18:14:46 +0900

    개선: 화면 깜빡임

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -1,16 +1,13 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
-  static async getStaticProps(ctx: DocumentContext) {
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -21,35 +18,15 @@ class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
-        styles: (
+        styles: [
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
-          </>
-        ),
+          </>,
+        ],
       }
     } finally {
       sheet.seal()
     }
   }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
 }
-
-export default MyDocument
